f97190d061150cf7e79cb2ae9e8918ec62470d12
Mark O'Connor <mark@myspotontheweb.com>
2014-01-30 01:22:28 +0000

    Removed unncessary java dependency declaration

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,6 @@ maintainer_email 'mark@myspotontheweb.com'
 license          'All rights reserved'
 description      'Installs/Configures demo'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
-depends "java"
 depends "tomcat"
